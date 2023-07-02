7541be9b5df62c1fead9cacd6cf708452d2abec6
Dell Pc <mudasir9757@gmail.com>
2023-07-03 01:35:46 +0500

    Fix linters in dark mode toggler and editor

--- assets/js/dark-mode-toggler.js
+++ assets/js/dark-mode-toggler.js
@@ -1,70 +1,69 @@
 function toggleDarkMode() { // jshint ignore:line
-	var toggler = document.getElementById( 'dark-mode-toggler' );
+  const toggler = document.getElementById('dark-mode-toggler');
 
-	if ( 'false' === toggler.getAttribute( 'aria-pressed' ) ) {
-		toggler.setAttribute( 'aria-pressed', 'true' );
-		document.documentElement.classList.add( 'is-dark-theme' );
-		document.body.classList.add( 'is-dark-theme' );
-		window.localStorage.setItem( 'twentytwentyoneDarkMode', 'yes' );
-	} else {
-		toggler.setAttribute( 'aria-pressed', 'false' );
-		document.documentElement.classList.remove( 'is-dark-theme' );
-		document.body.classList.remove( 'is-dark-theme' );
-		window.localStorage.setItem( 'twentytwentyoneDarkMode', 'no' );
-	}
+  if (toggler.getAttribute('aria-pressed') === 'false') {
+    toggler.setAttribute('aria-pressed', 'true');
+    document.documentElement.classList.add('is-dark-theme');
+    document.body.classList.add('is-dark-theme');
+    window.localStorage.setItem('twentytwentyoneDarkMode', 'yes');
+  } else {
+    toggler.setAttribute('aria-pressed', 'false');
+    document.documentElement.classList.remove('is-dark-theme');
+    document.body.classList.remove('is-dark-theme');
+    window.localStorage.setItem('twentytwentyoneDarkMode', 'no');
+  }
 }
 
 function twentytwentyoneIsDarkMode() {
-	var isDarkMode = window.matchMedia( '(prefers-color-scheme: dark)' ).matches;
+  let isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
 
-	if ( 'yes' === window.localStorage.getItem( 'twentytwentyoneDarkMode' ) ) {
-		isDarkMode = true;
-	} else if ( 'no' === window.localStorage.getItem( 'twentytwentyoneDarkMode' ) ) {
-		isDarkMode = false;
-	}
+  if (window.localStorage.getItem('twentytwentyoneDarkMode') === 'yes') {
+    isDarkMode = true;
+  } else if (window.localStorage.getItem('twentytwentyoneDarkMode') === 'no') {
+    isDarkMode = false;
+  }
 
-	return isDarkMode;
+  return isDarkMode;
 }
 
 function darkModeInitialLoad() {
-	var toggler = document.getElementById( 'dark-mode-toggler' ),
-		isDarkMode = twentytwentyoneIsDarkMode();
+  const toggler = document.getElementById('dark-mode-toggler');
+  const isDarkMode = twentytwentyoneIsDarkMode();
 
-	if ( isDarkMode ) {
-		document.documentElement.classList.add( 'is-dark-theme' );
-		document.body.classList.add( 'is-dark-theme' );
-	} else {
-		document.documentElement.classList.remove( 'is-dark-theme' );
-		document.body.classList.remove( 'is-dark-theme' );
-	}
+  if (isDarkMode) {
+    document.documentElement.classList.add('is-dark-theme');
+    document.body.classList.add('is-dark-theme');
+  } else {
+    document.documentElement.classList.remove('is-dark-theme');
+    document.body.classList.remove('is-dark-theme');
+  }
 
-	if ( toggler && isDarkMode ) {
-		toggler.setAttribute( 'aria-pressed', 'true' );
-	}
+  if (toggler && isDarkMode) {
+    toggler.setAttribute('aria-pressed', 'true');
+  }
 }
 
 function darkModeRepositionTogglerOnScroll() {
+  const toggler = document.getElementById('dark-mode-toggler');
+  let prevScroll = window.scrollY || document.documentElement.scrollTop;
+  let currentScroll;
 
-	var toggler = document.getElementById( 'dark-mode-toggler' ),
-		prevScroll = window.scrollY || document.documentElement.scrollTop,
-		currentScroll,
+  const checkScroll = function () {
+    currentScroll = window.scrollY || document.documentElement.scrollTop;
+    if (
+      currentScroll + (window.innerHeight * 1.5) > document.body.clientHeight
+				|| currentScroll < prevScroll
+    ) {
+      toggler.classList.remove('hide');
+    } else if (currentScroll > prevScroll && currentScroll > 250) {
+      toggler.classList.add('hide');
+    }
+    prevScroll = currentScroll;
+  };
 
-		checkScroll = function() {
-			currentScroll = window.scrollY || document.documentElement.scrollTop;
-			if (
-				currentScroll + ( window.innerHeight * 1.5 ) > document.body.clientHeight ||
-				currentScroll < prevScroll
-			) {
-				toggler.classList.remove( 'hide' );
-			} else if ( currentScroll > prevScroll && 250 < currentScroll ) {
-				toggler.classList.add( 'hide' );
-			}
-			prevScroll = currentScroll;
-		};
-
-	if ( toggler ) {
-		window.addEventListener( 'scroll', checkScroll );
-	}
+  if (toggler) {
+    window.addEventListener('scroll', checkScroll);
+  }
 }
 
 darkModeInitialLoad();
